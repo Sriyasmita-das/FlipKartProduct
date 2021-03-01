eadd935f4f2da5b600bf781d85b2426f8bb2b6c6
Sriyasmita-das <shri.shriyasmita32@gmail.com>
2021-03-01 22:40:55 +0530

    update

---
+++ src/main/java/com/flip/FlipKartProduct/FlipKartProductApplication.java
@@ -0,0 +1,13 @@
+package com.flip.FlipKartProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipKartProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipKartProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flip/FlipKartProduct/FlipKartProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flip.FlipKartProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipKartProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
